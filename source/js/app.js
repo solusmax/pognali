f9b1126844de5914427f04572686873bcd99af19
solusmax <solusmaxim@gmail.com>
2020-11-22 19:49:45 +0300

    Перенёс логику скрытия меню в блок page-header
    Чтобы остальные блоки вроде contacts можно было переиспользовать.
    Для этого также пришлось добавить уровень переопределения layout,
    чтобы сетка применялась в последнюю очередь.

--- source/js/app.js
+++ source/js/app.js
@@ -14,34 +14,36 @@ document.querySelector('.no-js').classList.remove('no-js');
 //                      //
 //////////////////////////
 
-const menuToggle = document.querySelector('.menu-toggle');
-const pageHeader = document.querySelector('.page-header');
-const siteNav = document.querySelector('.site-nav');
-const userNav = document.querySelector('.user-nav');
-const contacts = document.querySelector('.contacts');
-const contactsWrapper = document.querySelector('.page-header__contacts-wrapper');
-const socials = document.querySelector('.socials');
-const socialsWrapper = document.querySelector('.page-header__socials-wrapper');
+if (document.querySelector('.page-header')) {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const pageHeader = document.querySelector('.page-header');
+  const siteNav = document.querySelector('.page-header__site-nav');
+  const userNav = document.querySelector('.page-header__user-nav');
+  const contacts = document.querySelector('.page-header__contacts');
+  const contactsWrapper = document.querySelector('.page-header__contacts-wrapper');
+  const socials = document.querySelector('.page-header__socials');
+  const socialsWrapper = document.querySelector('.page-header__socials-wrapper');
 
-menuToggle.addEventListener('click', function(evt) {
-  evt.preventDefault();
+  menuToggle.addEventListener('click', function(evt) {
+    evt.preventDefault();
 
-  menuToggle.classList.toggle('menu-toggle--open');
-  pageHeader.classList.toggle('page-header--open');
-  siteNav.classList.toggle('site-nav--open');
-  userNav.classList.toggle('user-nav--open');
-  contacts.classList.toggle('contacts--open');
-  contactsWrapper.classList.toggle('page-header__contacts-wrapper--open');
-  socials.classList.toggle('socials--open');
-  socialsWrapper.classList.toggle('page-header__socials-wrapper--open');
-});
+    menuToggle.classList.toggle('menu-toggle--open');
+    pageHeader.classList.toggle('page-header--open');
+    siteNav.classList.toggle('page-header__site-nav--open');
+    userNav.classList.toggle('page-header__user-nav--open');
+    contacts.classList.toggle('page-header__contacts--open');
+    contactsWrapper.classList.toggle('page-header__contacts-wrapper--open');
+    socials.classList.toggle('page-header__socials--open');
+    socialsWrapper.classList.toggle('page-header__socials-wrapper--open');
+  });
 
-window.addEventListener("scroll", function() {
-  menuToggle.classList.add('menu-toggle--scroll');
-  pageHeader.classList.add('page-header--scroll');
+  window.addEventListener("scroll", function() {
+    menuToggle.classList.add('menu-toggle--scroll');
+    pageHeader.classList.add('page-header--scroll');
 
-  if (pageYOffset === 0) {
-    menuToggle.classList.remove('menu-toggle--scroll');
-    pageHeader.classList.remove('page-header--scroll');
-  }
-});
+    if (pageYOffset === 0) {
+      menuToggle.classList.remove('menu-toggle--scroll');
+      pageHeader.classList.remove('page-header--scroll');
+    };
+  });
+};
