3505749b2ce5dc27e971461d9e54f499358d4ef9
solusmax <solusmaxim@gmail.com>
2020-11-30 09:30:15 +0300

    Слегка причесал скрипт левела

--- source/js/app.js
+++ source/js/app.js
@@ -66,25 +66,34 @@ if (document.querySelector('.level')) {
   const levels = document.querySelectorAll('.level');
 
   for (const level of levels) {
-    const levelBar = level.querySelector('.level__bar');
+    const levelSvg = level.querySelector('.level__svg');
     const levelNumber = level.querySelector('.level__number').textContent;
-
     const levelSize = parseInt(getComputedStyle(level).getPropertyValue('--level-size'));
-    const levelBarWidth = parseInt(getComputedStyle(level).getPropertyValue('--level-bar-width'));
+    const levelBar = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
 
-    levelBar.setAttribute('cx', levelSize / 2);
-    levelBar.setAttribute('cy', levelSize / 2);
-    levelBar.setAttribute('r', (levelSize / 2) - (levelBarWidth / 2));
+    levelBar.classList.add('level__bar');
 
-    const levelBarLength = (levelBar.r.baseVal.value * 2) * Math.PI;
+    const setLevelBar = () => {
+      const levelBarWidth = parseInt(getComputedStyle(level).getPropertyValue('--level-bar-width'));
+      const levelBarPosition = levelSize / 2;
+      const levelBarRadius = (levelSize / 2) - (levelBarWidth / 2);
+      const levelBarLength = (levelBarRadius * 2) * Math.PI;
 
-    levelBar.style.strokeDasharray = levelBarLength;
+      levelBar.setAttribute('cx', levelBarPosition);
+      levelBar.setAttribute('cy', levelBarPosition);
+      levelBar.setAttribute('r', levelBarRadius);
+      levelBar.setAttribute('stroke-dasharray', levelBarLength);
 
-    if (levelNumber >= 90 && levelNumber <= 99) {
-      levelBar.style.strokeDashoffset = levelBarLength * ((90 + ((levelNumber - 90) / 2)) / 100) - levelBarLength;
-    } else {
-      levelBar.style.strokeDashoffset = levelBarLength * (levelNumber / 100) - levelBarLength;
+      if (levelNumber >= 90 && levelNumber <= 99) {
+        levelBar.setAttribute('stroke-dashoffset', levelBarLength * ((90 + ((levelNumber - 90) / 2)) / 100) - levelBarLength);
+      } else {
+        levelBar.setAttribute('stroke-dashoffset', levelBarLength * (levelNumber / 100) - levelBarLength);
+      };
     };
+
+    setLevelBar();
+
+    levelSvg.append(levelBar);
   };
 };
 
