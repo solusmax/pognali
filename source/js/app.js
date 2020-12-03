3cd24623429a3d812f0e12cfa8bb3b567d5933ee
solusmax <solusmaxim@gmail.com>
2020-12-03 19:09:50 +0300

    Сверстал mobile-версии «Моих планов» и двух шагов
    Первый и третий шаги.

--- source/js/app.js
+++ source/js/app.js
@@ -107,8 +107,8 @@ if (document.querySelector('.level')) {
 if (document.querySelector('.business-rates')) {
   const showBusinessRatesLink = document.querySelector('.new-profile__show-business-rates');
   const businessRatesModal = document.querySelector('.business-rates');
-  const businessRatesCloseButton = document.querySelector(".business-rates__close");
-  const businessRatesBackground =document.querySelector(".business-rates__background");
+  const businessRatesCloseButton = document.querySelector('.business-rates__close');
+  const businessRatesBackground =document.querySelector('.business-rates__background');
 
   // Открытие модалки
 
@@ -143,6 +143,77 @@ if (document.querySelector('.business-rates')) {
 
         closeBusinessRatesModal();
       };
+    };
+  });
+};
+
+//////////////////////////
+//                      //
+//   СЧЁТЧИКИ В ФОРМЕ   //
+//                      //
+//////////////////////////
+
+if (document.querySelector('.counter')) {
+  const companionsCounter = document.querySelector('#companions-counter');
+  const companionsCounterDecrease = document.querySelector('#companions-counter-decrease');
+  const companionsCounterIncrease = document.querySelector('#companions-counter-increase');
+  const durationDays = document.querySelector('#duration-days');
+  const durationDaysDecrease = document.querySelector('#duration-days-decrease');
+  const durationDaysIncrease = document.querySelector('#duration-days-increase');
+
+  // «Ищу попутчиков»
+
+  companionsCounterDecrease.addEventListener('click', function(evt) {
+    evt.preventDefault();
+
+    if (companionsCounter.value === "") {
+      companionsCounter.value = companionsCounter.min;
+    } else if (companionsCounter.value > companionsCounter.min) {
+      companionsCounter.value -= 1;
+    };
+  });
+
+  companionsCounterIncrease.addEventListener('click', function(evt) {
+    evt.preventDefault();
+
+    if (companionsCounter.value === "") {
+      companionsCounter.value = companionsCounter.min;
+    } else {
+      companionsCounter.value = parseInt(companionsCounter.value) + 1;
+    };
+  });
+
+  companionsCounter.addEventListener('input', function() {
+    if ((companionsCounter.value < companionsCounter.min) && companionsCounter.value !== "") {
+      companionsCounter.value = companionsCounter.min;
+    };
+  });
+
+  // «Длительность»
+
+  durationDaysDecrease.addEventListener('click', function(evt) {
+    evt.preventDefault();
+
+    if (durationDays.value === "") {
+      durationDays.value = durationDays.min;
+    } else if (durationDays.value > durationDays.min) {
+      durationDays.value -= 1;
+    };
+  });
+
+  durationDaysIncrease.addEventListener('click', function(evt) {
+    evt.preventDefault();
+
+    if (durationDays.value === "") {
+      durationDays.value = durationDays.min;
+    } else {
+      durationDays.value = parseInt(durationDays.value) + 1;
+    };
+  });
+
+  durationDays.addEventListener('input', function() {
+    if ((durationDays.value < durationDays.min) && durationDays.value !== "") {
+      durationDays.value = durationDays.min;
     };
   });
 };
