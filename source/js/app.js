1ac97425964c7d8b76c782be085d7198f22cab8d
solusmax <solusmaxim@gmail.com>
2020-12-08 21:03:00 +0300

    Оживил выбор буквы в «Фильтрации по странам»

--- source/js/app.js
+++ source/js/app.js
@@ -363,12 +363,18 @@ if (document.querySelector('.select')) {
 //////////////////////////
 
 if (document.querySelector('.countries-filter')) {
-  const countriesFilterToggle = document.querySelector('.countries-filter__toggle');
-  const countriesFilterClose = document.querySelector('.countries-filter__close')
-  const countriesFilterInner = document.querySelector('.countries-filter__inner');
-  const countriesFilterContinents = document.querySelector('.countries-filter__continents');
-  const countriesFilterLetters = document.querySelector('.countries-filter__letters');
-  const countriesFilterCountriesAll = document.querySelector('.countries-filter__countries-all');
+  const countriesFilter = document.querySelector('.countries-filter');
+  const countriesFilterToggle = countriesFilter.querySelector('.countries-filter__toggle');
+  const countriesFilterClose = countriesFilter.querySelector('.countries-filter__close')
+  const countriesFilterInner = countriesFilter.querySelector('.countries-filter__inner');
+  const countriesFilterContinents = countriesFilter.querySelector('.countries-filter__continents');
+  const countriesFilterLetters = countriesFilter.querySelector('.countries-filter__letters');
+  const countriesFilterCountriesAll = countriesFilter.querySelector('.countries-filter__countries-all');
+  const letters = countriesFilter.querySelectorAll('.countries-filter__letter');
+  const lettersFields = countriesFilter.querySelectorAll('.countries-filter__letter-field');
+  const countriesLists = countriesFilter.querySelectorAll('.countries-filter__countries-wrapper');
+
+  // Скрытие/открытие
 
   countriesFilterToggle.addEventListener('click', function(evt) {
     evt.preventDefault();
@@ -391,6 +397,28 @@ if (document.querySelector('.countries-filter')) {
     countriesFilterCountriesAll.classList.remove('countries-filter__countries-all--open');
     countriesFilterClose.classList.remove('countries-filter__close--open');
   });
+
+  // Выбор буквы
+
+  function clearCurrentCountries() {
+    for (let i = 0; i < letters.length; i += 1) {
+      letters[i].classList.remove('countries-filter__letter--current');
+      lettersFields[i].classList.remove('countries-filter__letter-field--current');
+      countriesLists[i].classList.remove('countries-filter__countries-wrapper--current');
+    };
+  };
+
+  for (let i = 0; i < letters.length; i += 1) {
+    lettersFields[i].addEventListener('click', function(evt) {
+      evt.preventDefault();
+
+      clearCurrentCountries();
+
+      letters[i].classList.add('countries-filter__letter--current');
+      lettersFields[i].classList.add('countries-filter__letter-field--current');
+      countriesLists[i].classList.add('countries-filter__countries-wrapper--current');
+    });
+  };
 }
 
 //////////////////////////
