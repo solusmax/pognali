4f0b33c719d8d051dd556d9eb50c1569d374e29c
solusmax <solusmaxim@gmail.com>
2020-12-08 20:23:55 +0300

    Сверстал мобильную версию «Подбора попутчика»

--- source/js/app.js
+++ source/js/app.js
@@ -392,3 +392,25 @@ if (document.querySelector('.countries-filter')) {
     countriesFilterClose.classList.remove('countries-filter__close--open');
   });
 }
+
+//////////////////////////
+//                      //
+//     «ПОДБЕРИТЕ       //
+//      ИДЕАЛЬНОГО      //
+//      ПОПУТЧИКА»      //
+//                      //
+//////////////////////////
+
+if (document.querySelector('.companions-filter')) {
+  const companionsFilterButtons = document.querySelectorAll('.companions-filter__block-button');
+  const companionsFilterContents = document.querySelectorAll('.companions-filter__content');
+
+  for (let i = 0; i < companionsFilterButtons.length; i += 1) {
+    companionsFilterButtons[i].addEventListener('click', function(evt) {
+      evt.preventDefault();
+
+      companionsFilterButtons[i].classList.toggle('companions-filter__block-button--open');
+      companionsFilterContents[i].classList.toggle('companions-filter__content--open');
+    });
+  };
+}
