4117086b8383257e5974beb88744e4a2de497a59
solusmax <solusmaxim@gmail.com>
2020-12-09 16:58:21 +0300

    Доверстал мобильную версию «Попутчиков»

--- source/js/app.js
+++ source/js/app.js
@@ -419,7 +419,7 @@ if (document.querySelector('.countries-filter')) {
       countriesLists[i].classList.add('countries-filter__countries-wrapper--current');
     });
   };
-}
+};
 
 //////////////////////////
 //                      //
@@ -441,4 +441,38 @@ if (document.querySelector('.companions-filter')) {
       companionsFilterContents[i].classList.toggle('companions-filter__content--open');
     });
   };
-}
+};
+
+//////////////////////////
+//                      //
+//         ЛАЙК         //
+//                      //
+//////////////////////////
+
+if (document.querySelector('.like')) {
+  const likeButtons = document.querySelectorAll('.like__button');
+  const likeTexts = document.querySelectorAll('.like__button span');
+  const likeCounters = document.querySelectorAll('.like__counter');
+
+  for (let i = 0; i < likeButtons.length; i += 1) {
+    likeButtons[i].addEventListener('click', function(evt) {
+      evt.preventDefault();
+
+      likeButtons[i].classList.toggle('like__button--liked');
+
+      if (likeButtons[i].classList.contains('like__button--liked')) {
+        likeTexts[i].textContent = 'Снять лайк';
+
+        if (!(/[^\d]/.test(likeCounters[i].textContent))) {
+          likeCounters[i].textContent = parseInt(likeCounters[i].textContent) + 1;
+        };
+      } else {
+        likeTexts[i].textContent = 'Поставить лайк';
+
+        if (!(/[^\d]/.test(likeCounters[i].textContent))) {
+          likeCounters[i].textContent = parseInt(likeCounters[i].textContent) - 1;
+        };
+      };
+    });
+  };
+};
