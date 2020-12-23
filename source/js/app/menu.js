d02fd69deed777a52c99263601113a4f59fa967c
solusmax <solusmaxim@gmail.com>
2020-12-23 03:52:24 +0300

    Сверстал десктопную версию шапки

--- source/js/app/menu.js
+++ source/js/app/menu.js
@@ -9,7 +9,10 @@
 if (document.querySelector('.page-header')) {
   const menuToggle = document.querySelector('.menu-toggle');
   const pageHeader = document.querySelector('.page-header');
-  const siteNav = pageHeader.querySelector('.page-header__site-nav');
+  const pageHeaderInner = pageHeader.querySelector('.page-header__inner');
+  const pageHeaderSiteNav = pageHeader.querySelector('.page-header__site-nav');
+  const siteNav = document.querySelector('.site-nav');
+  const siteNavLinks = document.querySelectorAll('.site-nav__link');
   const userNav = pageHeader.querySelector('.page-header__user-nav');
   const contacts = pageHeader.querySelector('.page-header__contacts');
   const contactsWrapper = pageHeader.querySelector('.page-header__contacts-wrapper');
@@ -22,8 +25,8 @@ if (document.querySelector('.page-header')) {
     evt.preventDefault();
 
     menuToggle.classList.toggle('menu-toggle--open');
-    pageHeader.classList.toggle('page-header--open');
-    siteNav.classList.toggle('page-header__site-nav--open');
+    pageHeaderInner.classList.toggle('page-header__inner--open');
+    pageHeaderSiteNav.classList.toggle('page-header__site-nav--open');
     userNav.classList.toggle('page-header__user-nav--open');
     contacts.classList.toggle('page-header__contacts--open');
     contactsWrapper.classList.toggle('page-header__contacts-wrapper--open');
@@ -36,6 +39,11 @@ if (document.querySelector('.page-header')) {
   function scrolledMenu () {
     menuToggle.classList.add('menu-toggle--scroll');
     pageHeader.classList.add('page-header--scroll');
+    pageHeaderInner.classList.add('page-header__inner--scroll');
+    siteNav.classList.add('site-nav--scroll');
+    for (let siteNavLink of siteNavLinks) {
+      siteNavLink.classList.add('site-nav__link--scroll')
+    };
     logo.classList.add('logo--scroll');
     logoPicture.classList.add('logo__picture--scroll');
   };
@@ -43,6 +51,11 @@ if (document.querySelector('.page-header')) {
   function notScrolledMenu () {
     menuToggle.classList.remove('menu-toggle--scroll');
     pageHeader.classList.remove('page-header--scroll');
+    pageHeaderInner.classList.remove('page-header__inner--scroll');
+    siteNav.classList.remove('site-nav--scroll');
+    for (let siteNavLink of siteNavLinks) {
+      siteNavLink.classList.remove('site-nav__link--scroll')
+    };
     logo.classList.remove('logo--scroll');
     logoPicture.classList.remove('logo__picture--scroll');
   }
