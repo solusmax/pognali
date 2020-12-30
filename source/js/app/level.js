8c10d46e581c726743e5159462ad84827fe05858
solusmax <solusmaxim@gmail.com>
2020-12-31 02:16:59 +0300

    Сверстал планшетную версию блока «Профиль»

--- source/js/app/level.js
+++ source/js/app/level.js
@@ -12,12 +12,12 @@ if (document.querySelector('.level')) {
   for (const level of levels) {
     const levelSvg = level.querySelector('.level__svg');
     const levelNumber = level.querySelector('.level__number').textContent;
-    const levelSize = parseInt(getComputedStyle(level).getPropertyValue('--level-size'));
     const levelBar = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
 
     levelBar.classList.add('level__bar');
 
     const setLevelBar = () => {
+      const levelSize = parseInt(getComputedStyle(level).getPropertyValue('--level-size'));
       const levelBarWidth = parseInt(getComputedStyle(level).getPropertyValue('--level-bar-width'));
       const levelBarPosition = levelSize / 2;
       const levelBarRadius = (levelSize / 2) - (levelBarWidth / 2);
@@ -36,6 +36,10 @@ if (document.querySelector('.level')) {
     };
 
     setLevelBar();
+
+    window.addEventListener('resize', function() {
+      setLevelBar();
+    });
 
     levelSvg.append(levelBar);
   };
