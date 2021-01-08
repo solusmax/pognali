efee2edb07433ac4af89473e4d1580843c4e8be1
solusmax <solusmaxim@gmail.com>
2021-01-08 14:32:21 +0300

    Сверстал планшетную версию «Фильтрации по странам»

--- source/js/app/countries-filter.js
+++ source/js/app/countries-filter.js
@@ -10,11 +10,14 @@
 if (document.querySelector('.countries-filter')) {
   const countriesFilter = document.querySelector('.countries-filter');
   const countriesFilterToggle = countriesFilter.querySelector('.countries-filter__toggle');
+  const countriesFilterToggleText = countriesFilterToggle.querySelector('.countries-filter__toggle-text');
   const countriesFilterClose = countriesFilter.querySelector('.countries-filter__close')
   const countriesFilterInner = countriesFilter.querySelector('.countries-filter__inner');
   const countriesFilterContinents = countriesFilter.querySelector('.countries-filter__continents');
   const countriesFilterLetters = countriesFilter.querySelector('.countries-filter__letters');
   const countriesFilterCountriesAll = countriesFilter.querySelector('.countries-filter__countries-all');
+  const pageMainCountriesFilterInner = document.querySelector('.page-main__countries-filter-inner');
+
   const letters = countriesFilter.querySelectorAll('.countries-filter__letter');
   const lettersFields = countriesFilter.querySelectorAll('.countries-filter__letter-field');
   const countriesLists = countriesFilter.querySelectorAll('.countries-filter__countries-wrapper');
@@ -24,12 +27,19 @@ if (document.querySelector('.countries-filter')) {
   countriesFilterToggle.addEventListener('click', function(evt) {
     evt.preventDefault();
 
+    if (countriesFilterToggle.classList.contains('countries-filter__toggle--open')) {
+      countriesFilterToggleText.textContent = countriesFilterToggle.dataset.textOpen;
+    } else {
+      countriesFilterToggleText.textContent = countriesFilterToggle.dataset.textClose;
+    };
+
     countriesFilterToggle.classList.toggle('countries-filter__toggle--open');
     countriesFilterInner.classList.toggle('countries-filter__inner--open');
     countriesFilterContinents.classList.toggle('countries-filter__continents--open');
     countriesFilterLetters.classList.toggle('countries-filter__letters--open');
     countriesFilterCountriesAll.classList.toggle('countries-filter__countries-all--open');
     countriesFilterClose.classList.toggle('countries-filter__close--open');
+    pageMainCountriesFilterInner.classList.toggle('page-main__countries-filter-inner--open');
   });
 
   countriesFilterClose.addEventListener('click', function(evt) {
@@ -41,6 +51,7 @@ if (document.querySelector('.countries-filter')) {
     countriesFilterLetters.classList.remove('countries-filter__letters--open');
     countriesFilterCountriesAll.classList.remove('countries-filter__countries-all--open');
     countriesFilterClose.classList.remove('countries-filter__close--open');
+    pageMainCountriesFilterInner.classList.remove('page-main__countries-filter-inner--open');
   });
 
   // Выбор буквы
